72db60c5c534821221b9e721253f93c377d2f161
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-07-31 18:09:45 +0530

    nested routes fixed

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import TopBar from "./TopBar";
 import Dashboard from "./Dashboard";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <TopBar />
       <Dashboard />
+      <Outlet /> 
     </>
   );
 };
