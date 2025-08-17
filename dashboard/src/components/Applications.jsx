294b283b2ec9909e3469d80875deee606e535b23
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-08-17 17:30:18 +0530

    changed application page

--- dashboard/src/components/Applications.jsx
+++ dashboard/src/components/Applications.jsx
@@ -1,7 +1,7 @@
 
 function App() {
   return ( 
-    <h1 className="text-center text-2xl text-blue-200">This is App.</h1>
+    <h1 className="text-center text-2xl text-blue-900">Comming Soon!</h1>
    );
 }
 
