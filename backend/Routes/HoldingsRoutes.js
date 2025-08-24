7edc9f361f72bd1a763a305295dea8808f75ec8d
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-08-24 23:04:08 +0530

    fixed holdingRoute

--- backend/Routes/HoldingsRoutes.js
+++ backend/Routes/HoldingsRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { userVerification } = require("../Middleware/AuthMiddleware");
-const ctrl = require("../Controllers/HoldingsController");
+const ctrl = require("../Controllers/HoldingController");
 
 router.get("/", userVerification, ctrl.list);
 router.post("/", userVerification, ctrl.create);
