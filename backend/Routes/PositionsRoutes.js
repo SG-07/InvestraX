d34e165a80c8f3e8cb581464560a5f3caf7b62bd
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-08-24 23:07:02 +0530

    fixed positionRoutes file

--- backend/Routes/PositionsRoutes.js
+++ backend/Routes/PositionsRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { userVerification } = require("../Middleware/AuthMiddleware");
-const ctrl = require("../Controllers/PositionsController");
+const ctrl = require("../Controllers/PositionController");
 
 router.get("/", userVerification, ctrl.list);
 
