41eafe47b1c7add75410a01e8a5cb1328b19b81d
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-08-24 23:12:07 +0530

    routes fixed

--- backend/Routes/WatchlistRoutes.js
+++ backend/Routes/WatchlistRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { userVerification } = require("../Middleware/AuthMiddleware");
-const ctrl = require("../Controllers/WatchlistController");
+const ctrl = require("../Controllers/WatchListController");
 
 router.get("/", userVerification, ctrl.list);
 router.post("/", userVerification, ctrl.add);
